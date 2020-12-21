dcffdae50b42701fdc9c244ede1e0e6fa4a5f515
bronya96 <bronyazaychik96@gmail.com>
2020-12-22 06:04:37 +0700

    add some endPoint recipe (and Like, Comment, Bookmark)

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -43,4 +43,279 @@ module.exports = {
       });
     });
   },
+  updateRecipe: () => {
+
+  },
+  getAllRecipes: () => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `SELECT id_recipe, img, title FROM tb_recipe WHERE is_showed = 1`
+      db.query(queryStr, (err, data) => {
+        if (!err) {
+          // console.log(data)
+          // console.log('resolve')
+          resolve({
+            status: 200,
+            message: `berhasil menampilkan data`,
+            data: data
+          })
+          // resolve(data)
+        } else {
+          console.log('reject')
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  getRecipesByUser: (id) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `SELECT id_recipe, img, title FROM tb_recipe WHERE user_id = ?`
+      db.query(queryStr, user_id, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `Berhasil menampilkan data`,
+            data: data
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  addLike: (user_id, recipe_id) => {
+    const body = {
+      user_id: user_id,
+      recipe_id: recipe_id
+    }
+    return new Promise((resolve, reject) => {
+      const queryStr = `INSERT INTO tb_like_recipe SET ?`
+      db.query(queryStr, body, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `Recipe ${recipe_id} has been liked`,
+            likedId: data.insertId
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  getLikedRecipe: (user_id) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `SELECT lr.id as "like_Id", r.id_recipe, r.img, r.title FROM tb_like_recipe lr JOIN tb_recipe r ON lr.recipe_id = r.id_recipe WHERE lr.user_id = ?`
+      db.query(queryStr, user_id, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `Liked`,
+            data: data
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  unlikeFromDetail: (user_id, recipe_id) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `DELETE FROM tb_like_recipe WHERE user_id = ? AND recipe_id = ?`
+      db.query(queryStr, [user_id, recipe_id], (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `unliked`
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  unlikeFromList: (likedId) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `DELETE FROM tb_like_recipe WHERE id = ?`
+      db.query(queryStr, likedId, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `unliked ${likedId}`
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  addBookmark: (user_id, recipe_id) => {
+    const body = {
+      user_id: user_id,
+      recipe_id: recipe_id
+    }
+    return new Promise((resolve, reject) => {
+      const queryStr = `INSERT INTO tb_bookmark_recipe SET ?`
+      db.query(queryStr, body, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `Recipe ${recipe_id}  has been bookmarked`,
+            bookmarkId: data.insertId
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  getBookmarkedRecipe: (user_id) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `SELECT br.id as "bookmark_id", r.id_recipe, r.img, r.title FROM tb_bookmark_recipe br JOIN tb_recipe r ON br.recipe_id = r.id_recipe WHERE br.user_id = ?`
+      db.query(queryStr, user_id, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `Bookmark`,
+            data: data
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  removeBookmarkFromDetail: (user_id, recipe_id) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `DELETE FROM tb_bookmark_recipe WHERE user_id = ? AND recipe_id = ?`
+      db.query(queryStr, [user_id, recipe_id], (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `unbookmark`
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  removeBookmarkFromList: (bookmarkId) => {
+    return new Promise((resolve, reject) => {
+      const queryStr = `DELETE FROM tb_bookmark_recipe WHERE id = ?`
+      db.query(queryStr, bookmarkId, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `remove Bookmark : ${bookmarkId}`
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  addComment: (user_id, recipeId, comment) => {
+    const body = {
+      user_id: user_id,
+      recipe_id: recipeId,
+      comment: comment
+    }
+    return new Promise((resolve, reject) => {
+      const queryStr = `INSERT INTO tb_comment_recipe SET ?`
+      db.query(queryStr, body, (err, data) => {
+        if (!err) {
+          resolve({
+            status: 200,
+            message: `You commented ${comment} on recipe ${recipeId}`,
+            // commented: data.insertId
+          })
+        } else {
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  getRecipeComment: (recipeId) => {
+    return new Promise ((resolve, reject) => {
+      const queryStr = `SELECT r.id_recipe, r.title, u.name, c.comment FROM tb_comment_recipe c JOIN tb_user u ON c.user_id = u.id_user JOIN tb_recipe r ON r.id_recipe = c.recipe_id WHERE c.recipe_id = ?`
+      db.query(queryStr, recipeId, (err, data) =>{
+        if(!err){
+          resolve({
+            recipeId: data[0].id_recipe,
+            recipeName: data[0].title,
+            data:data //hhehe
+          })
+        }else{
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  },
+  deleteRecipe: (recipeId) => {
+    return new Promise ((resolve, reject) => {
+      const queryStr = `DELETE FROM tb_recipe WHERE id_recipe = ?`
+      db.query(queryStr, recipeId, (err, data) =>{
+        if(!err){
+          resolve({
+            status:200,
+            message:`Successfully deleted`
+          })
+        }else{
+          reject({
+            status: 500,
+            message: `Encountered error`,
+            details: err
+          })
+        }
+      })
+    })
+  }
 };
--- src/routes/recipeRouter.js
+++ src/routes/recipeRouter.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const recipeController = require ('../controllers/recipeController')
 const recipeRouter = express.Router()
-const multiUpload = require("../helpers/upload");
+const multiUpload = require("../helpers/upload_old");
 
 // const checkToken = require ('./../helpers/checkToken')
 
 recipeRouter.post('/add', multiUpload, recipeController.addRecipe)
+recipeRouter.get ('/', recipeController.getAllRecipes)
+recipeRouter.delete('/delete/:recipeId', recipeController.deleteRecipe)
+
+//Like Recipe
+recipeRouter.post('/like/:recipeId', recipeController.likeRecipe)
+recipeRouter.get ('/likedRecipe', recipeController.getLikedRecipe)
+//fromRecipe
+recipeRouter.delete('/unlike/:recipeId',recipeController.unlikeFromDetail)
+//fromList
+recipeRouter.delete('/unlikeList/:likedId',recipeController.unlikeFromList)
+
+//Bookmark Recipe
+recipeRouter.post('/bookmark/:recipeId', recipeController.bookmarkRecipe)
+recipeRouter.get ('/bookmarkedRecipe', recipeController.getBookmarkedRecipe)
+//fromRecipe
+recipeRouter.delete('/removeBookmark/:recipeId',recipeController.removeBookmarkFromDetail)
+recipeRouter.delete('/removebookmarklist/:bookmarkId',recipeController.removeBookmarkFromList)
+
+//Comment Recipe
+recipeRouter.post('/addComment/:recipeId',recipeController.addComment)
+recipeRouter.get('/comment/:recipeId', recipeController.getCommentRecipe)
+
+
+
 
 module.exports = recipeRouter
