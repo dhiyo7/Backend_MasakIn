c09e328db6e7b30227fa69d886e00e4416986083
bronya96 <bronyazaychik96@gmail.com>
2020-12-21 09:40:36 +0700

    add some end point of recipe

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mainRouter = express.Router()
 
 const authRouter = require ('./authRouter')
+const welcomeRouter = require ('./welcome')
 
 // const checkToken = require('./../helpers/checkToken')
 
 //MasakIn
-
+mainRouter.use("/", welcomeRouter)
 mainRouter.use("/auth", authRouter)
 
 
---
+++ src/routes/recipeRouter.js
@@ -0,0 +1,3 @@
+const express = require('express')
+const recipeController = require ('./../controllers/recipeController')
+const recipeController = express.Router()
